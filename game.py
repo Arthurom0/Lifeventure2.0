553b77ef1924a8bfce1deb0c5c56e69a6e4e750d
Arthurom0 <123201001+Arthurom0@users.noreply.github.com>
2023-05-06 11:18:05 +0200

    bonne version du menu principal

--- game.py
+++ game.py
@@ -232,7 +232,4 @@ def jeuprincipal():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()                           
-                    
-
-jeuprincipal()        
 
