aacc2b44549eee50414c0876f8cc5e6cb8214fa8
Arthurom0 <123201001+Arthurom0@users.noreply.github.com>
2023-05-10 18:50:36 +0200

    c bon

---
+++ constants.py
@@ -0,0 +1,24 @@
+import pygame
+pygame.init()
+BASE_DOSSIER = "res/"
+IMAGES_FOND = [
+    BASE_DOSSIER + "decors/Paysage3.png",
+    BASE_DOSSIER + "decors/Map_Temple.png",
+    BASE_DOSSIER + "decors/Paysage_4.png",
+]
+MUSIQUE_FOND = BASE_DOSSIER + "musiques/Musique_donjon.mp3"
+DOSSIER_ANIM_JEUNE = BASE_DOSSIER + "anim_players/jeune/"
+ANIM_JEUNE = DOSSIER_ANIM_JEUNE + "Jeune.png"
+DOSSIER_ENNEMI = BASE_DOSSIER + "anim_players/cactus/"
+ENNEMI_MOMIE =  BASE_DOSSIER + "anim_players/momie/"
+MOMIE = ENNEMI_MOMIE + "momie_idle.png"
+ENNEMI_CACTUS = DOSSIER_ENNEMI + "Cactus.png"
+HEALTH = BASE_DOSSIER + "coeur/Coeur.png"
+MENU = BASE_DOSSIER + "menu/menu.png"
+DEATH  = BASE_DOSSIER + "Ecran_de_mort.png"
+ENNNEMI_BOSS = BASE_DOSSIER + 'anim_players/Boss'
+BOSS = ENNNEMI_BOSS + "/Boss_atk.png"
+
+infoObject = pygame.display.Info()
+width, height = infoObject.current_w, infoObject.current_h
+
